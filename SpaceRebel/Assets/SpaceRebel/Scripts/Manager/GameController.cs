b75449739eb0a31a4b1db08333c61d1b7608f5ea
Manmeet <manmeet@onjoyride.com>
2022-02-25 20:45:54 +0530

    Basic set up of the menu scene. Added basic game menu settings. Added Game Manager and UImanager classes to handle comunication between game.

---
+++ SpaceRebel/Assets/SpaceRebel/Scripts/Manager/GameController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public enum GAMESTATE
+{
+    UI = 0,
+    GAMEPLAY = 1
+}
+public enum AUDIOTYPE
+{
+    BUTTON,
+    PLAYERSHOT,
+    EXPLOSION
+}
+
+/// <summary>
+/// Scripts handle the game state and the sound.
+/// </summary>
+
+public class GameController : MonoBehaviour
+{
+    #region PUBLIC FIELDS
+
+    [HideInInspector] public bool isAudioOff = false;
+
+    [HideInInspector] public bool isNebulaTheme = false;
+
+    public const string highestScoreKey = "HighestScore";
+
+    #endregion
+
+    #region PRIVATE FIELDS
+
+    SoundManager soundManager;
+
+    static GameController instance;
+
+    AudioSource source;
+
+    GAMESTATE currentGameState;
+
+    #endregion
+
+    #region UNITY METHODS
+
+    private void Awake()
+    {
+        instance = this;
+
+        DontDestroyOnLoad(this);
+
+        source = GetComponent<AudioSource>();
+
+        soundManager = GetComponent<SoundManager>();
+    }
+
+    #endregion
+
+    public static GameController GetInstance()
+    {
+        if (instance == null)
+        {
+            GameObject gameManager = new GameObject("GameManager");
+
+            instance = gameManager.AddComponent<GameController>();
+        }
+        return instance;
+    }
+
+    #region PUBLIC METHODS
+
+    public GAMESTATE GetGameState()
+    {
+        return currentGameState;
+    }
+
+    // Set the game current states
+    public void SetGameState(GAMESTATE state)
+    {
+        currentGameState = state;
+    }
+
+    // Plays common audios
+    public void PlayAudio(AUDIOTYPE type, bool isLoop = false, float volume = 1)
+    {
+        source.mute = isAudioOff;
+        soundManager.PlayAudio(type, isAudioOff, isLoop, volume);
+    }
+    #endregion
+}
---
+++ SpaceRebel/Assets/SpaceRebel/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    #region SERIALIZE FIELDS
+
+    [SerializeField] AudioClip buttonAudio;
+
+    [SerializeField] AudioClip playerShootAudio;
+
+    [SerializeField] AudioClip explosionAudio;
+
+    #endregion
+
+    #region PRIVATE FIELDS
+    AudioSource source;
+    #endregion
+    // Plays common audios
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    public void PlayAudio(AUDIOTYPE type, bool isAudioOff,bool isLoop = false,float volume=1)
+    {
+        source.mute = isAudioOff;
+        switch (type)
+        {
+            case AUDIOTYPE.BUTTON:
+                source.clip = buttonAudio;
+                break;
+
+            case AUDIOTYPE.PLAYERSHOT:
+                source.clip = playerShootAudio;
+                break;
+
+            case AUDIOTYPE.EXPLOSION:
+                source.clip = explosionAudio;
+                break;
+        }
+
+        source.volume = volume;
+        source.loop = isLoop;
+        source.Play();
+    }
+}
